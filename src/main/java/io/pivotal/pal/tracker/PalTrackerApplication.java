ea0854ad0c09abfc2e7845e7d1a25e9bb66c28c7
Wellson Lee <wellsonleeyy@hotmail.com>
2019-09-24 13:58:02 +0800

    Lab8

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -1,20 +1,26 @@
 package io.pivotal.pal.tracker;
 
+import com.mysql.cj.jdbc.MysqlDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 
 @SpringBootApplication
 public class PalTrackerApplication {
 
-
+    private JdbcTemplate jdbcTemplate;
     public static void main(String[] args) {
         SpringApplication.run(PalTrackerApplication.class, args);
     }
 
     @Bean
     TimeEntryRepository timeEntryRepository() {
-        return new InMemoryTimeEntryRepository();
+
+        MysqlDataSource dataSource = new MysqlDataSource();
+        dataSource.setUrl(System.getenv("SPRING_DATASOURCE_URL"));
+        //jdbcTemplate = new JdbcTemplate(dataSource);
+        return new JdbcTimeEntryRepository(dataSource);
     }
 }
